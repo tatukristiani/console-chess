b9b98e2e1bd545aeb6f2575e3e4eefd3fbc5fd97
Tatu Pulkkinen <tatu.pulkkinen@metropolia.fi>
2024-02-27 20:06:49 +0200

    init

---
+++ console-chess/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace console_chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Create chess board
+            ChessBoard board = new ChessBoard();
+            board.PrintBoard();
+
+            // Matriisi, array, 
+
+
+            // TODO: Add chess pieces to board
+
+        }
+    }
+}
